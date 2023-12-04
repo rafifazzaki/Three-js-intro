4d7ea9fa6eea05c1083445cd50dc920d43122c3c
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2023-12-04 16:32:55 +0700

    donw javascript, attempting TWEEN

--- first-portofolio/main.js
+++ first-portofolio/main.js
@@ -5,8 +5,10 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { degToRad } from 'three/src/math/MathUtils';
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+import TWEEN from '@tweenjs/tween.js'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(20, 15, 20);
 
 
@@ -29,7 +31,7 @@ const gridHelper = new THREE.GridHelper(200, 50)
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enablePan = false;
 controls.enableZoom = false;
-scene.add(gridHelper)
+// scene.add(gridHelper)
 scene.add(controls)
 
 // const light = new THREE.AmbientLight(0xffffff, 10)
@@ -46,6 +48,39 @@ scene.add(plane)
 
 plane.rotateX(degToRad(-90))
 
+
+
+
+var cv_page = document.getElementById('CV_click');
+var porto_page = document.getElementById('Porto_click');
+var home_click = document.getElementById('Home_click');
+cv_page.onclick = showCV;
+porto_page.onclick = showPortofolio;
+home_click.onclick = clearPage
+
+function clearPage(){
+  document.getElementById("PAGE_CV").setAttribute("style", "display: none");
+  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
+}
+
+function showPortofolio () {
+  document.getElementById("PAGE_CV").setAttribute("style", "display: none");
+  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: block");
+}
+
+function showCV(){
+  document.getElementById("PAGE_CV").setAttribute("style", "display: block");
+  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
+
+  new TWEEN.Tween(controls.target).to({
+    x: 0,
+    y: 0,
+    z: 0
+  }, 500)
+  .easing(TWEEN.Easing.Cubic.Out)
+  .start()
+
+}
 
 
 function animate(){
