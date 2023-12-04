a8504f35c4e6497e5f79affea8ca8d48b68e76dc
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2023-12-04 16:05:42 +0700

    working onclick

---
+++ first-portofolio/page.js
@@ -0,0 +1,10 @@
+function showPortofolio(){
+  
+    document.getElementById("PAGE_CV").setAttribute("style", "display: none");
+    document.getElementById("PAGE_Portofolio").setAttribute("style", "display: block");
+  }
+  
+  function showCV(){
+  document.getElementById("PAGE_CV").setAttribute("style", "display: block");
+  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
+  }
