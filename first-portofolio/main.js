0af0fe6f81e22bfd2e81ddcdb010fb4a9e8aa57e
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2023-12-09 23:24:12 +0700

    working clickable button and cannon js intro

--- first-portofolio/main.js
+++ first-portofolio/main.js
@@ -4,34 +4,41 @@ import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { degToRad } from 'three/src/math/MathUtils';
-
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import TWEEN, { Tween } from '@tweenjs/tween.js'
+
+
+// 
+// // init
+// 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(20, 15, 20);
 
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 })
-
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.render(scene, camera);
 
 
 
+// 
+// // geometry
+// 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 120)
-const material = new THREE.MeshBasicMaterial({color:0xE58943, wireframe:true});
+const material = new THREE.MeshBasicMaterial({color:0xE58943, wireframe:true}); //0x818589
 const torus = new THREE.Mesh(geometry, material)
 // torus.rotateY(degToRad(45))
-scene.add(torus);
+// scene.add(torus);
 
 const gridHelper = new THREE.GridHelper(200, 50)
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enablePan = false;
-controls.enableZoom = false;
+// controls.enableZoom = false;
 controls.enableDamping = true
+controls.minPolarAngle = Math.PI/8; // radians
 controls.maxPolarAngle = Math.PI/2; 
 // scene.add(gridHelper)
 scene.add(controls)
@@ -51,19 +58,57 @@ scene.add(plane)
 
 plane.rotateX(degToRad(-90))
 
+const loader = new GLTFLoader();
+
+loader.load(
+  'base.glb',
+  function(gltf){
+    const base = gltf.scene
+    // var newMaterial = new THREE.MeshBasicMaterial({color: 0xE58943, wireframe: false})
+    // base.traverse((o) => {
+    //   if(o.isMesh) o.material = newMaterial;
+    // })
+    base.scale.set(7, 7, 7);
+    base.position.setY(-1.5)
+    scene.add(base);
+  }
+)
+
 
 
 
 var cv_page = document.getElementById('CV_click');
-var porto_page = document.getElementById('Porto_click');
-var home_click = document.getElementById('Home_click');
+// var porto_page = document.getElementById('Porto_click');
+var home_page = document.getElementById('Home_click');
+var back_button = document.getElementById('Back_button');
 cv_page.onclick = showCV;
-porto_page.onclick = showPortofolio;
-home_click.onclick = clearPage
+// porto_page.onclick = showPortofolio;
+home_page.onclick = clearPage;
+back_button.onclick = goBack;
+
+
+function goBack(){
+  document.getElementById("PAGE_CV").setAttribute("style", "display: none");
+  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
+  document.getElementById("container").setAttribute("style", "position: static;");
+  
+  var t = new TWEEN.Tween(controls.target).to(
+    {
+      x: 0,
+      y: 0,
+      z: 0
+    }, 1000)
+    .easing(TWEEN.Easing.Quadratic.Out)
+    .start()
+    .onComplete(function(){
+      controls.enableRotate = true;
+    })
+}
 
 function clearPage(){
   document.getElementById("PAGE_CV").setAttribute("style", "display: none");
   document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
+  document.getElementById("container").setAttribute("style", "position: static;");
   
   var t = new TWEEN.Tween(controls.target).to(
     {
@@ -88,11 +133,13 @@ function clearPage(){
 function showPortofolio () {
   document.getElementById("PAGE_CV").setAttribute("style", "display: none");
   document.getElementById("PAGE_Portofolio").setAttribute("style", "display: block");
+  document.getElementById("container").setAttribute("style", "position: relative;");
 }
 
 function showCV(){
   document.getElementById("PAGE_CV").setAttribute("style", "display: block");
   document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
+  document.getElementById("container").setAttribute("style", "position: relative;");
   controls.enableRotate = false;
 
 
@@ -148,4 +195,113 @@ animate();
 
 
 
+let raycaster = new THREE.Raycaster();
+let mouse = new THREE.Vector2();
+let INTERSECTED;
+renderer.domElement.addEventListener('mousemove', onPointerMove, false) //pointermove
+window.addEventListener('resize', onWindowResize);
+renderer.domElement.addEventListener('click', onPointerClick, false)
+// renderer.domElement.addEventListener('',)
+
+
+function onWindowResize(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onPointerMove(event){
+  mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+  mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObject(scene, true);
+
+  if ( intersects.length > 0 ) {
+
+    if ( INTERSECTED != intersects[ 0 ].object ) {
+
+      if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+
+      INTERSECTED = intersects[ 0 ].object;
+      INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+      switch(INTERSECTED.name) {
+        case "button1":
+          GLOW();
+          break;
+        case "button2":
+          GLOW();
+          break;
+        case "button3":
+          GLOW();
+          break;
+        case "button4":
+          GLOW();
+          break;
+        case "button5":
+          GLOW();
+          break;
+        case "button6":
+          GLOW();
+          break;
+        
+        default:
+          // code block
+      }
+      
+      // INTERSECTED.material.color.setHex( 0xff0000 );
+
+    }
+
+  } else {
+
+    if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+
+    INTERSECTED = null;
+
+  }
+  function GLOW(){
+    // INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+    INTERSECTED.material.color.setHex( 0xff0000 );
+  }
+}
+
+function onPointerClick( event ) {
+  mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+  mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObject(scene, true);
+
+  if(intersects.length > 0){
+    
+    switch(intersects[ 0 ].object.name) {
+      case "button1":
+        CONTENT_button1();
+        break;
+      case "button2":
+        CONTENT_button1();
+        break;
+      case "button3":
+        CONTENT_button1();
+        break;
+      case "button4":
+        CONTENT_button1();
+        break;
+      case "button5":
+        CONTENT_button1();
+        break;
+      case "button6":
+        CONTENT_button1();
+        break;
+      
+      default:
+        // code block
+    }
+  }
+
+  function CONTENT_button1(){
+    console.log("asdasd");
+  }
+
+}
 
---
+++ three-cannon-es/main.js
@@ -0,0 +1,90 @@
+import './style.css'
+import * as THREE from 'three';
+
+import * as CANNON from 'cannon-es';
+import CannonDebugger from 'cannon-es-debugger';
+
+// 
+// // init: three js
+// 
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 10);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera);
+
+
+
+
+// 
+// // init: cannon js
+// 
+
+const physicsWorld = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.82, 0)
+})
+
+const groundBody = new CANNON.Body({
+  type: CANNON.Body.STATIC,
+  shape: new CANNON.Plane() //infinite plane
+})
+groundBody.quaternion.setFromEuler(-Math.PI/2, 0, 0);
+physicsWorld.addBody(groundBody);
+
+// debugger
+const cannonDebugger = new CannonDebugger(scene, physicsWorld,{})
+
+// 
+// // Body: cannon js
+// 
+const radius = 1;
+const sphereBody = new CANNON.Body({
+  mass: 5,
+  shape: new CANNON.Sphere(radius)
+});
+sphereBody.position.set(0, 7, 0);
+physicsWorld.addBody(sphereBody);
+
+const boxBody = new CANNON.Body({
+  mass: 5,
+  shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+})
+boxBody.position.set(1, 10, 0);
+physicsWorld.addBody(boxBody);
+
+// 
+// // Geometry: three js
+// 
+
+const sphereGeo = new THREE.SphereGeometry(radius)
+const sphereMat = new THREE.MeshBasicMaterial({color:0xE58943}); //0x818589
+const sphere = new THREE.Mesh(sphereGeo, sphereMat)
+// torus.rotateY(degToRad(45))
+scene.add(sphere);
+
+const boxGeo = new THREE.BoxGeometry(2, 2, 2) //double from cannon
+const boxMat = new THREE.MeshBasicMaterial()
+const box = new THREE.Mesh(boxGeo, boxMat)
+scene.add(box)
+
+
+
+function animate(){
+  physicsWorld.fixedStep();
+  cannonDebugger.update()
+  requestAnimationFrame(animate)
+  // update three js from cannon
+  sphere.position.copy(sphereBody.position);
+  sphere.quaternion.copy(sphereBody.quaternion)
+  box.position.copy(boxBody.position);
+  box.quaternion.copy(boxBody.quaternion);
+
+  renderer.render(scene, camera);
+}
+
+animate()
