863b641eac2d01c597ecfbe6fad2bb74bd83580d
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2023-12-10 19:06:11 +0700

    CANNON works
    CANNON JS Body and THREE JS Mesh is done, and collider on the area is set

--- first-portofolio/main.js
+++ first-portofolio/main.js
@@ -1,6 +1,8 @@
 import './style.css'
 
 import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
+import CannonDebugger from 'cannon-es-debugger';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { degToRad } from 'three/src/math/MathUtils';
@@ -22,26 +24,126 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.render(scene, camera);
 
-
-
-// 
-// // THREE JS GEOMETRY
-// 
-
-const geometry = new THREE.TorusGeometry(10, 3, 16, 120)
-const material = new THREE.MeshBasicMaterial({color:0xE58943, wireframe:true}); //0x818589
-const torus = new THREE.Mesh(geometry, material)
-// torus.rotateY(degToRad(45))
-// scene.add(torus);
-const gridHelper = new THREE.GridHelper(200, 50)
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enablePan = false;
 // controls.enableZoom = false;
 controls.enableDamping = true
 controls.minPolarAngle = Math.PI/8; // radians
 controls.maxPolarAngle = Math.PI/2; 
-// scene.add(gridHelper)
 scene.add(controls)
+
+// 
+// // INIT CANNON JS
+// 
+
+const physicsWorld = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.82, 0)
+})
+
+const groundBody = new CANNON.Body({
+  type: CANNON.Body.STATIC,
+  shape: new CANNON.Plane()
+})
+
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
+groundBody.position.set(0, -1.5, 0)
+physicsWorld.addBody(groundBody)
+
+const cannonDebugger = new CannonDebugger(scene, physicsWorld)
+
+// 
+// // CANNON JS BODY
+// 
+
+const shape = CANNON.Trimesh.createTorus(50, 3, 4, 8);
+const body = new CANNON.Body({ mass: 20 });
+body.addShape(shape);
+body.position.set(0, 1, 0)
+body.quaternion.setFromEuler(-Math.PI/2, 0, 0)
+physicsWorld.addBody(body)
+
+var radius = 1
+const sphereBody = new CANNON.Body({
+  mass: 5,
+  shape: new CANNON.Sphere(radius)
+})
+sphereBody.position.set(2, 7, 0)
+physicsWorld.addBody(sphereBody)
+
+const sphereBody2 = new CANNON.Body({
+  mass: 5,
+  shape: new CANNON.Sphere(radius)
+})
+sphereBody.position.set(0, 7, 2)
+physicsWorld.addBody(sphereBody2)
+
+const boxBody = new CANNON.Body({
+  mass: 5,
+  shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+})
+boxBody.position.set(1, 10, 1)
+physicsWorld.addBody(boxBody)
+
+const boxBody2 = new CANNON.Body({
+  mass: 5,
+  shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+})
+boxBody.position.set(1, 10, 1)
+physicsWorld.addBody(boxBody2)
+
+var CYLINDER_HEIGHT = 5
+const coneBody = new CANNON.Body({
+  mass: 5,
+  shape: new CANNON.Cylinder(0.01, 3, CYLINDER_HEIGHT, 4, 1)
+})
+coneBody.position.set(0, 10, 0)
+physicsWorld.addBody(coneBody)
+
+const longBoxBody = new CANNON.Body({
+  mass: 5,
+  shape: new CANNON.Box(new CANNON.Vec3(2, 0.2, 2))
+})
+longBoxBody.position.set(0, 4, 0)
+physicsWorld.addBody(longBoxBody)
+
+// 
+// // THREE JS GEOMETRY
+// 
+
+const sphereGeo = new THREE.SphereGeometry(radius)
+
+const sphereMat = new THREE.MeshBasicMaterial({color:0xE58943}); //0x818589
+const sphere = new THREE.Mesh(sphereGeo, sphereMat)
+
+const sphereMat2 = new THREE.MeshBasicMaterial({color:0xE58943}); //0x818589
+const sphere2 = new THREE.Mesh(sphereGeo, sphereMat2)
+scene.add(sphere);
+scene.add(sphere2);
+
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(2, 2, 2),
+  new THREE.MeshBasicMaterial()
+)
+scene.add(box)
+
+const box2 = new THREE.Mesh(
+  new THREE.BoxGeometry(2, 2, 2),
+  new THREE.MeshBasicMaterial()
+)
+scene.add(box2)
+
+const cone = new THREE.Mesh(
+  new THREE.ConeGeometry(3, 5, 4),
+  new THREE.MeshBasicMaterial()
+)
+scene.add(cone)
+
+const longBox = new THREE.Mesh(
+  new THREE.BoxGeometry(4, 0.4, 4),
+  new THREE.MeshBasicMaterial()
+)
+scene.add(longBox)
+
 
 // const light = new THREE.AmbientLight(0xffffff, 10)
 const light = new THREE.PointLight(0xffffff, 20)
@@ -49,30 +151,30 @@ const lightHelper = new THREE.PointLightHelper(light)
 light.position.setY(5)
 scene.add(light, lightHelper)
 
+
 const plane = new THREE.Mesh(
   new THREE.PlaneGeometry(200, 200),
   new THREE.MeshStandardMaterial()
 )
 plane.position.setY(-1.5)
+plane.rotateX(degToRad(-90))
 scene.add(plane)
 
-plane.rotateX(degToRad(-90))
+// const loader = new GLTFLoader();
 
-const loader = new GLTFLoader();
-
-loader.load(
-  'base.glb',
-  function(gltf){
-    const base = gltf.scene
-    // var newMaterial = new THREE.MeshBasicMaterial({color: 0xE58943, wireframe: false})
-    // base.traverse((o) => {
-    //   if(o.isMesh) o.material = newMaterial;
-    // })
-    base.scale.set(7, 7, 7);
-    base.position.setY(-1.5)
-    scene.add(base);
-  }
-)
+// loader.load(
+//   'base.glb',
+//   function(gltf){
+//     const base = gltf.scene
+//     // var newMaterial = new THREE.MeshBasicMaterial({color: 0xE58943, wireframe: false})
+//     // base.traverse((o) => {
+//     //   if(o.isMesh) o.material = newMaterial;
+//     // })
+//     base.scale.set(7, 7, 7);
+//     base.position.setY(-1.5)
+//     scene.add(base);
+//   }
+// )
 
 
 // 
@@ -87,7 +189,6 @@ cv_page.onclick = showCV;
 // porto_page.onclick = showPortofolio;
 home_page.onclick = clearPage;
 back_button.onclick = goBackPage;
-
 
 function goBackPage(){
   document.getElementById("PAGE_CV").setAttribute("style", "display: none");
@@ -178,17 +279,7 @@ function checkQuadrant(objectPosition){
 }
 
 
-function animate(){
-  requestAnimationFrame(animate)
 
-  controls.update();
-
-  TWEEN.update()
-
-  renderer.render(scene, camera);
-}
-
-animate();
 
 
 
@@ -306,3 +397,39 @@ function onPointerClick( event ) {
 
 }
 
+
+function animate(){
+  physicsWorld.fixedStep()
+  cannonDebugger.update()
+  requestAnimationFrame(animate)
+
+
+  sphere.position.copy(sphereBody.position)
+  sphere.quaternion.copy(sphereBody.quaternion)
+
+  sphere2.position.copy(sphereBody2.position)
+  sphere2.quaternion.copy(sphereBody2.quaternion)
+
+
+  box.position.copy(boxBody.position)
+  box.quaternion.copy(boxBody.quaternion)
+
+  box2.position.copy(boxBody2.position)
+  box2.quaternion.copy(boxBody2.quaternion)
+
+
+  cone.position.copy(coneBody.position)
+  cone.quaternion.copy(coneBody.quaternion)
+
+  longBox.position.copy(longBoxBody.position)
+  longBox.quaternion.copy(longBoxBody.quaternion)
+
+
+
+// three
+  controls.update();
+  TWEEN.update()
+  renderer.render(scene, camera);
+}
+
+animate();
