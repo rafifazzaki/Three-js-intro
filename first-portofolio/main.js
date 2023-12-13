274703ba6fb0cdca74f0e107acbf65547053b8a2
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2023-12-13 17:03:47 +0700

    scroll logic works
    removal of page logic,
    scroll logic to add to scene is working (somehow).
    there is an interesting behaviour, if we use if(scrollPercentage >= 15 && < 30) and else to remove it, it will only exist on that scrollPercentage range and retain it's last position before disappeared

--- first-portofolio/main.js
+++ first-portofolio/main.js
@@ -14,7 +14,7 @@ import TWEEN, { Tween } from '@tweenjs/tween.js'
 
 
 // 
-// // INIT THREE JS
+// // INIT THREE JS & SETTING
 // 
 
 const scene = new THREE.Scene();
@@ -34,6 +34,15 @@ controls.enableDamping = true
 controls.minPolarAngle = Math.PI/8; // radians
 controls.maxPolarAngle = Math.PI/2; 
 scene.add(controls)
+
+
+function onWindowResize(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize);
 
 // 
 // // INIT CANNON JS
@@ -57,11 +66,10 @@ const cannonDebugger = new CannonDebugger(scene, physicsWorld)
 // 
 // // CANNON JS BODY
 // 
-
 const shape = CANNON.Trimesh.createTorus(50, 3, 4, 8);
-const body = new CANNON.Body({ mass: 20 });
+const body = new CANNON.Body({ mass: 1000 });
 body.addShape(shape);
-body.position.set(0, 1, 0)
+body.position.set(0, 1.5, 0)
 body.quaternion.setFromEuler(-Math.PI/2, 0, 0)
 physicsWorld.addBody(body)
 
@@ -71,48 +79,49 @@ const sphereBody = new CANNON.Body({
   shape: new CANNON.Sphere(radius)
 })
 sphereBody.position.set(2, 7, 0)
-physicsWorld.addBody(sphereBody)
+// physicsWorld.addBody(sphereBody)
 
 const sphereBody2 = new CANNON.Body({
   mass: 5,
   shape: new CANNON.Sphere(radius)
 })
 sphereBody.position.set(0, 7, 2)
-physicsWorld.addBody(sphereBody2)
+// physicsWorld.addBody(sphereBody2)
 
 const boxBody = new CANNON.Body({
   mass: 5,
   shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1))
 })
 boxBody.position.set(1, 10, 1)
-physicsWorld.addBody(boxBody)
+// physicsWorld.addBody(boxBody)
 
 const boxBody2 = new CANNON.Body({
   mass: 5,
   shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1))
 })
 boxBody.position.set(1, 10, 1)
-physicsWorld.addBody(boxBody2)
+// physicsWorld.addBody(boxBody2)
 
 var CYLINDER_HEIGHT = 5
 const coneBody = new CANNON.Body({
   mass: 5,
   shape: new CANNON.Cylinder(0.01, 3, CYLINDER_HEIGHT, 4, 1)
 })
-coneBody.position.set(0, 10, 0)
-physicsWorld.addBody(coneBody)
+coneBody.position.set(0, 13, 0)
+// physicsWorld.addBody(coneBody)
 
 const longBoxBody = new CANNON.Body({
   mass: 5,
   shape: new CANNON.Box(new CANNON.Vec3(2, 0.2, 2))
 })
-longBoxBody.position.set(0, 4, 0)
-physicsWorld.addBody(longBoxBody)
+longBoxBody.position.set(0, 13, 0)
+// physicsWorld.addBody(longBoxBody)
 
 // 
 // // THREE JS GEOMETRY
 // 
 
+// FLAG_three object
 const sphereGeo = new THREE.SphereGeometry(radius)
 
 const sphereMat = new THREE.MeshBasicMaterial({color:0xE58943}); //0x818589
@@ -120,32 +129,32 @@ const sphere = new THREE.Mesh(sphereGeo, sphereMat)
 
 const sphereMat2 = new THREE.MeshBasicMaterial({color:0xE58943}); //0x818589
 const sphere2 = new THREE.Mesh(sphereGeo, sphereMat2)
-scene.add(sphere);
-scene.add(sphere2);
+// scene.add(sphere);
+// scene.add(sphere2);
 
 const box = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial()
 )
-scene.add(box)
+// scene.add(box)
 
 const box2 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial()
 )
-scene.add(box2)
+// scene.add(box2)
 
 const cone = new THREE.Mesh(
   new THREE.ConeGeometry(3, 5, 4),
   new THREE.MeshBasicMaterial()
 )
-scene.add(cone)
+// scene.add(cone)
 
 const longBox = new THREE.Mesh(
   new THREE.BoxGeometry(4, 0.4, 4),
   new THREE.MeshBasicMaterial()
 )
-scene.add(longBox)
+// scene.add(longBox)
 
 
 // const light = new THREE.AmbientLight(0xffffff, 10)
@@ -180,123 +189,12 @@ scene.add(plane)
 // )
 
 
-// 
-// // PAGE CONTENT
-// 
-
-var cv_page = document.getElementById('CV_click');
-// var porto_page = document.getElementById('Porto_click');
-var home_page = document.getElementById('Home_click');
-var back_button = document.getElementById('Back_button');
-cv_page.onclick = showCV;
-// porto_page.onclick = showPortofolio;
-home_page.onclick = clearPage;
-back_button.onclick = goBackPage;
-
-function goBackPage(){
-  document.getElementById("PAGE_CV").setAttribute("style", "display: none");
-  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
-  document.getElementById("container").setAttribute("style", "position: static;");
-  
-  var t = new TWEEN.Tween(controls.target).to(
-    {
-      x: 0,
-      y: 0,
-      z: 0
-    }, 1000)
-    .easing(TWEEN.Easing.Quadratic.Out)
-    .start()
-    .onComplete(function(){
-      controls.enableRotate = true;
-    })
-}
-function clearPage(){
-  document.getElementById("PAGE_CV").setAttribute("style", "display: none");
-  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
-  document.getElementById("container").setAttribute("style", "position: static;");
-  
-  var t = new TWEEN.Tween(controls.target).to(
-    {
-      x: 0,
-      y: 0,
-      z: 0
-    }, 1000)
-    .easing(TWEEN.Easing.Quadratic.Out)
-    .start()
-    .onComplete(function(){
-      controls.enableRotate = true;
-    })
-
-  new TWEEN.Tween(camera.position).to(new THREE.Vector3(20, 15, 20), 1000)
-  .easing(TWEEN.Easing.Cubic.Out)
-  .start()
-  .onStart(function(){
-    controls.enableRotate = false;
-  })
-}
-function showPortofolio () {
-  document.getElementById("PAGE_CV").setAttribute("style", "display: none");
-  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: block");
-  document.getElementById("container").setAttribute("style", "position: relative;");
-}
-function showCV(){
-  document.getElementById("PAGE_CV").setAttribute("style", "display: block");
-  document.getElementById("PAGE_Portofolio").setAttribute("style", "display: none");
-  document.getElementById("container").setAttribute("style", "position: relative;");
-  controls.enableRotate = false;
-
-
-  new TWEEN.Tween(controls.target).to({
-    x: camera.position.x - 1 * checkQuadrant(camera.position).x,
-    y: camera.position.y - 5,
-    z: camera.position.z - 1 * checkQuadrant(camera.position).y
-  }, 1000)
-  .easing(TWEEN.Easing.Cubic.Out)
-  .start()
-
-  // new TWEEN.Tween(camera.position).to({
-  //   x: camera.position.x + 10 * checkQuadrant(camera.position).x,
-  //   y: camera.position.y,
-  //   z: camera.position.z + 10 * checkQuadrant(camera.position).y
-  // }, 1000)
-  // .easing(TWEEN.Easing.Cubic.Out)
-  // .start()
-  
-}
-function checkQuadrant(objectPosition){
-  let number;
-  if(objectPosition.x > 0 && objectPosition.z > 0){
-    // kuadran 1
-    number = new THREE.Vector2(1, 1);
-  }else if(objectPosition.x < 0 && objectPosition.z > 0){
-    // kuadran 2
-    number = new THREE.Vector2(-1, 1);
-  }else if(objectPosition.x < 0 && objectPosition.z < 0){
-    // kuadran 3
-    number = new THREE.Vector2(-1, -1);
-  }else if(objectPosition.x > 0 && objectPosition.z < 0){
-    // kuadran 4
-    number = new THREE.Vector2(1, -1);
-  }
-  return number
-}
-
-
-
-
-
 
 // 
 // SCENE INTERACTION
 // 
 
-function onWindowResize(){
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-window.addEventListener('resize', onWindowResize);
 
 
 
@@ -338,6 +236,10 @@ function animate(){
 animate();
 
 
+// 
+// scroll listener
+// 
+
 document.addEventListener('DOMContentLoaded', function () {
   const circles = document.querySelectorAll('.circle');
 
@@ -348,6 +250,71 @@ document.addEventListener('DOMContentLoaded', function () {
 
       // circle.style.backgroundColor = `hsl(200, 100%, ${fillPercentage}%)`;
       circle.style.backgroundColor = `rgba(255, 255, 255, ${fillPercentage / 20})`;
+
+      let switcher1, switcher2, switcher3, switcher4, switcher5, switcher6
+      switcher1 = switcher2 = switcher3 = switcher4 = switcher5 = switcher6 = false;
+
+      switcher1 = true;
+      if (scrollPercentage >= 15 && scrollPercentage < 30) {
+        physicsWorld.addBody(sphereBody)
+        scene.add(sphere);
+        // console.log("1");
+        // stopper1 = true
+        // switcher1 = true;
+      }else{
+        physicsWorld.removeBody(sphereBody)
+        scene.remove(sphere)
+      }
+       if(scrollPercentage >= 30){
+        physicsWorld.addBody(sphereBody2)
+        scene.add(sphere2);
+      } else {
+        physicsWorld.removeBody(sphereBody2)
+        scene.remove(sphere2)
+      }
+
+      if(scrollPercentage >= 45){
+        physicsWorld.addBody(boxBody)
+        scene.add(box)
+      } 
+      else{
+        physicsWorld.removeBody(boxBody)
+        scene.remove(box)
+      }
+      if(scrollPercentage >= 60){
+        physicsWorld.addBody(boxBody2)
+        scene.add(box2)
+      } 
+      else if(switcher1 = true){
+        physicsWorld.removeBody(boxBody2)
+        scene.remove(box2)
+      }
+      if(scrollPercentage >= 75){
+        physicsWorld.addBody(coneBody)
+        scene.add(cone)
+      }else{
+        physicsWorld.removeBody(coneBody)
+        scene.remove(cone)
+      }
+
+      if(scrollPercentage >= 90){
+        physicsWorld.addBody(longBoxBody)
+        scene.add(longBox)
+      }else{
+        physicsWorld.removeBody(longBoxBody)
+        scene.remove(longBox)
+        // switcher1 = false
+      }
+
+      console.clear();
+      scene.traverse( function( object ) {
+        
+        if(object instanceof THREE.Mesh){
+          console.log(object)
+      }
+      
+      } );
+
     });
   }
 
